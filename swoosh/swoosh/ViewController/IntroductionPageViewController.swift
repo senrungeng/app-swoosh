3ed0ad6ba8dbb0753f351d869718f5753a21367d
Senrun Geng <senrungeng222@gmail.com>
2017-12-13 21:13:35 -0500

    3D added

--- swoosh/swoosh/ViewController/IntroductionPageViewController.swift
+++ swoosh/swoosh/ViewController/IntroductionPageViewController.swift
@@ -7,22 +7,47 @@
 //
 
 import UIKit
+import SceneKit
 
 class IntroductionPageViewController: UIViewController {
 
+    @IBOutlet weak var boxview: SCNView!
+    
     @IBOutlet weak var IntroBackground: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        sceneSetup()
         IntroBackground.alpha = 0.2
 
         // Do any additional setup after loading the view.
     }
 
+    func sceneSetup() {
+        // 1
+        let scene = SCNScene()
+        
+        // 2
+        let boxGeometry = SCNBox(width: 2, height: 2, length: 2, chamferRadius: 0.2)
+        let boxNode = SCNNode(geometry: boxGeometry)
+        scene.rootNode.addChildNode(boxNode)
+        
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3Make(0, 0, 10)
+        scene.rootNode.addChildNode(cameraNode)
+        // 3
+        boxview.scene = scene
+        boxview.autoenablesDefaultLighting = true
+        boxview.allowsCameraControl = true
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
     /*
